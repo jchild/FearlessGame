ae4280d85f49b02804b0482b717f3be274051513
cjpeters2012 <cjpeters2012@gmail.com>
2016-01-18 20:50:41 -0800

    First Commit? I dont know GIT

---
+++ desktop/src/com/fearless/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fearless.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fearless.game.FearlessGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FearlessGame(), config);
+	}
+}
