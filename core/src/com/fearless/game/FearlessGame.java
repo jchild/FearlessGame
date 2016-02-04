f8645a49c2b58947cda90ee38a9851b1dbf2593e
cjpeters2012 <cjpeters2012@gmail.com>
2016-02-03 19:43:06 -0800

    -Removing Grade.prefs files -Adding random learning code to FearlessGame.java

--- core/src/com/fearless/game/FearlessGame.java
+++ core/src/com/fearless/game/FearlessGame.java
@@ -2,26 +2,59 @@ package com.fearless.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FearlessGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private BitmapFont font;
+	private Texture img;
+	private Pixmap pixmap;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		font.setColor(Color.GOLDENROD);
+		
+		//A pixmap is basically a raw image in memory as represented by pixels
+		//We create one 256 wide, 128 height using 8 bytes for RGBA channels
+		pixmap = new Pixmap(246, 128, Pixmap.Format.RGBA8888);
+		
+		//Make it Red?
+		pixmap.setColor(Color.RED);
+		pixmap.fill();
+		
+		//Draw two lines forming an X
+		pixmap.setColor(Color.BLACK);
+		pixmap.drawLine(0, 0, pixmap.getWidth()-1, pixmap.getHeight()-1);
+		pixmap.drawLine(0, pixmap.getHeight()-1, pixmap.getHeight()-1, 0);
+		
+		//Draw circle?
+		pixmap.setColor(Color.YELLOW);
+		pixmap.drawCircle(pixmap.getWidth()/2, pixmap.getHeight()/2, pixmap.getHeight()/2 -1);
+		
+		img = new Texture(pixmap);
+		
+//		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
+		
 		batch.draw(img, 0, 0);
+		font.draw(batch,  "Hello World", 200, 200);
+		
+		
 		batch.end();
 	}
 }
